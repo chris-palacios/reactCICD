295fee9118daa25bef2450aaa35170095819a2a2
Christian Palacios <79600301+chris-palacios@users.noreply.github.com>
2026-07-20 13:37:41 -0700

    Added linting rules and adjusted yml to run linting on push

--- reactCICD/eslint.config.js
+++ reactCICD/eslint.config.js
@@ -1,10 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const js = require('@eslint/js');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
   },
+    
+  { files: ['**/*.ts', '**/*.tsx'], plugins: {js}, extends: ["js/recommended"] },
 ]);
